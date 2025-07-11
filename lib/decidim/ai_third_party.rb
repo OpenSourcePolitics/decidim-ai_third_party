18e4ce356c4dbed8fc1125f0b7a0979786b3ea50
Quentin Champenois <quentin.champenois7@gmail.com>
2025-07-11 14:16:54 +0200

    fix: Initial commit

---
+++ lib/decidim/ai_third_party.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ai_third_party/version"
+
+module Decidim
+  module AiThirdParty
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/decidim/ai_third_party_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Decidim::AiThirdParty do
+  it "has a version number" do
+    expect(Decidim::AiThirdParty::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
