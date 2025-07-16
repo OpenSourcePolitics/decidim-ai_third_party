6c3930d4bd7613fa97b70eef9b9c99f80a6c8e8d
Quentin Champenois <quentin.champenois7@gmail.com>
2025-07-16 12:05:34 +0200

    fix: Add openai strategy

--- lib/decidim/ai_third_party.rb
+++ lib/decidim/ai_third_party.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require "decidim/ai"
+
 require_relative "ai_third_party/version"
+require_relative "ai/spam_detection/openai/strategy"
 require_relative "ai_third_party/strategy/third_party"
 require_relative "ai_third_party/strategy/scaleway"
 
